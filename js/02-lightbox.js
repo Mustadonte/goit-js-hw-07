c4402dc83fd86ed53dcda6b99ae4cf6d3f7e143f
Ivan Zabolotniy <mustadonte@gmail.com>
2022-05-23 11:32:42 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -12,7 +12,7 @@ function createGalleryMarkup(items) {
   const galleryMarkup = items
     .map(
       (item) => `<li><a class="gallery__item" href=${item.original}>
-  <img class="gallery__image" src=${item.preview} alt=${item.description} />
+  <img class="gallery__image" src=${item.preview} alt="${item.description}" />
 </a></li>`
     )
     .join("");
